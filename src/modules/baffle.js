98f906200abac6ba818e00c8451950a92fa44221
Ian Emsens <ianemsens@gmail.com>
2018-10-29 00:14:54 +0100

    Added stylesheet, placeholder content and burst-baffle

--- src/modules/baffle.js
+++ src/modules/baffle.js
@@ -4,17 +4,34 @@ import baffle from '../../node_modules/baffle';
 import elements from './elements';
 
 // All code related to web elements I want baffle'd
-export const b = baffle(elements.baffle(), {
-    speed: 75,
-    characters: '01',
+const characters = '░▒▓';
+
+export const baffleElements = baffle(elements.baffle(), {
+    speed: 80,
+    characters: characters,
 });
 
+export const burstElements = baffle(elements.baffle(), {
+    speed: 25,
+    characters: characters,
+})
+
 export function init() {
-    b.start().reveal(1000, 2000);
+    const duration = 1000;
+    const delay = 2 * 1000;
+    baffleElements.start().reveal(duration, delay);
+}
+
+export function burst() {
+    const duration = 350;
+    const delay = 150;
+    burstElements.start().reveal(duration, delay);
 }
 
 // Prevents warning in webpack
 export default {
-    b: b,
+    baffleElements: baffleElements,
+    burstElements: burstElements,
     init: init,
+    burst: burst,
 };
