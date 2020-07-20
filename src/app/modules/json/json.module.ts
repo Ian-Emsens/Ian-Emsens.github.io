a6c0f7262e5ddd47df2dcb5f61acb1ee59f98988
Ian-Emsens <ianemsens@gmail.com>
2020-07-21 00:22:47 +0200

    Created json module

---
+++ src/app/modules/json/json.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ViewerComponent } from './components/viewer/viewer.component';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ViewerComponent
+  ]
+})
+export class JsonModule { }
