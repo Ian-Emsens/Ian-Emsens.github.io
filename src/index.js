053d1e7399c12c14ae89f878f8f0e334cd338399
Ian-Emsens <ianemsens@gmail.com>
2020-07-20 23:25:12 +0200

    wipe

--- src/index.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.info('[DOM] DOMContentLoaded');
-});
--- src/modules/elements.js
+++
@@ -1,9 +0,0 @@
-console.info('[Elements] Init');
-
-// A central collection of the elements I want adjusted on the page.
-
-export const elements = {
-}
-
-// Prevents warning in webpack
-export default elements;
