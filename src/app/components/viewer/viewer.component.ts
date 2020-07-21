755769998eaf3cdfce5066b81cf19543bd40eee9
Ian-Emsens <ianemsens@gmail.com>
2020-07-21 02:14:37 +0200

    Added initial state binding

--- src/app/components/viewer/viewer.component.ts
+++ src/app/components/viewer/viewer.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import { State } from 'src/app/reducers';
+import { State as CsvState } from 'src/app/reducers/csv.reducer';
+
+import { loadCSV } from 'src/app/actions/csv';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-viewer',
@@ -7,15 +14,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewerComponent implements OnInit {
 
-  public portfolio: any;
+  public state$: Observable<CsvState>;
 
-  constructor() {
-    this.portfolio = {
-      foo: 'bar',
-    }
+  constructor(private store: Store<State>) {
+    this.state$ = store.select('csv');
   }
 
   ngOnInit(): void {
+    this.store.dispatch(loadCSV({
+      path: 'data/test.csv',
+      name: 'test'
+    }));
   }
 
 }
